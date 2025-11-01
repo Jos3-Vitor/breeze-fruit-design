5ccaa1e383a16af8d337f58054e1fb24777ee32b
José Vitor Giardini Chagas <jv.giardinichagas@gmail.com>
2025-11-01 16:59:49 -0300

    correções vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/breeze-fruit-design/",
   server: {
     host: "::",
     port: 8080,
